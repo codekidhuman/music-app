8f29f82542233c0b6be62b5cc413c77e6fe1d2a9
codekidhuman <96500127+codekidhuman@users.noreply.github.com>
2022-09-17 15:30:08 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+lwX = 0;
+lwY = 0;
+rwX = 0;
+rwY = 0;
+lwS = 0;
+rwS = 0;
+stat1 = "";
+stat2 = "";
+
+var song1 = "";
+var song2 = "";
+
+function preload()
+{
+    song1 = loadSound("music.mp3");
+    song2 = loadSound("music_2.mp3");
+}
+function setup()
+{
+    canvas = createCanvas(600, 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+function draw()
+{
+    image(video, 0, 0, 600, 500);
+    stat1 = song1.isPlaying();
+    stat2 = song2.isPlaying();
+    console.log(stat1);
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    if (lwS > 0.2)
+    {
+        circle(lwX, lwY, 40);
+        song2.stop();
+        if(stat1 == false)
+        {
+            song1.play();
+            document.getElementById("song_name").innerHTML = "Cool Song";
+        }
+    }
+    else if (rwS > 0.2)
+    {
+        circle(rwX, rwY, 40);
+        song1.stop();
+        if(stat2 == false)
+        {
+            song2.play();
+            document.getElementById("song_name").innerHTML = "Nice tune";
+        }
+    }
+}
+function modelLoaded()
+{
+    console.log("PoseNet Model Has Been Loaded!")
+}
+function gotPoses(results, error)
+{
+    if (error)
+    {
+        console.error(error);
+    }
+    if(results.length>0)
+    {
+        lwS = results[0].pose.keypoints[9].score;
+        rwS = results[0].pose.keypoints[10].score;
+
+        lwX = results[0].pose.leftWrist.x
+        lwY = results[0].pose.leftWrist.y
+        rwX = results[0].pose.rightWrist.x
+        rwY = results[0].pose.rightWrist.y
+        console.log("Left Wrist Coordinates: "+lwX+", "+lwY);
+        console.log("Right Wrist Coordinates: "+rwX+", "+rwY);
+    }
+}
